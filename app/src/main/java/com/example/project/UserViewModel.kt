224d6efd4d26f55cf212a586f9d016876edfc0ab
sarafarahabadi <sarafarahabadi@gmail.com>
2022-03-17 23:06:48 +0200

    Signup and login works

---
+++ app/src/main/java/com/example/project/UserViewModel.kt
@@ -0,0 +1,39 @@
+package com.example.project
+
+
+import android.util.Log
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class UserViewModel: ViewModel() {
+    var username = mutableStateOf("")
+
+    fun createUser( emailRegister: String, pwRegister: String) {
+        Firebase.auth
+            .createUserWithEmailAndPassword(emailRegister, pwRegister)
+            .addOnSuccessListener {
+                username.value = emailRegister
+            }
+            .addOnFailureListener{ e ->
+                Log.d("error:", e.message.toString())
+            }
+    }
+
+    fun loginUser( emailLogin: String, pwLogin: String) {
+        Firebase.auth
+            .signInWithEmailAndPassword(emailLogin, pwLogin)
+            .addOnSuccessListener {
+                username.value = emailLogin
+            }
+            .addOnFailureListener{ e ->
+                Log.d("error:", e.message.toString())
+            }
+    }
+
+    fun logoutUser() {
+        Firebase.auth.signOut()
+        username.value = ""
+    }
+}
